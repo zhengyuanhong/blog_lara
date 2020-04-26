3a4d992a7eee5f15a46a29004d711c57ed847ed6
zhengyuanhong <1713639570@qq.com>
2020-04-26 23:24:45 +0800

    'fix'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //每周删本地图片文件
-        $schedule->command('z:remove')->weekly();
+//        $schedule->command('z:remove')->weekly();
     }
 
     /**
