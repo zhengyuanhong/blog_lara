558523a54ba37caab5e4440adef55f9409c9b87b
zhengyuanhong <1713639570@qq.com>
2020-05-12 16:29:47 +0800

    'fix'

--- app/Http/Controllers/User/WalletController.php
+++ app/Http/Controllers/User/WalletController.php
@@ -14,16 +14,17 @@ class WalletController extends Controller
     public function recharge(Request $request,OrderService $orderService,PayService $payService){
         $money = $request->get('money',1);
         $user = $request->user();
-        list($pay_param,$order) = $orderService->store($user,$money);
-        //检查权限
-        $this->authorize('own',$order);
-        //支付
-        $res = $payService->pay($pay_param);
         //检查钱包
         if(!$payService->walletCheckSign($user->wallet->toArray())){
             Log::info('账户异常'.__FILE__);
             return Response()->json(['code'=>201,'data'=>'账户异常']);
         }
+
+        list($pay_param,$order) = $orderService->store($user,$money);
+        //检查权限
+        $this->authorize('own',$order);
+        //支付
+        $res = $payService->pay($pay_param);
         return Response()->json(['code'=>200,'data'=>$res['url']]);
     }
 }
